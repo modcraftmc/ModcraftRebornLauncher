84aaaa74a5b4ea9293f9ac63d6d01a6af1821902
goldor <dimgoldoritri@gmail.com>
2023-12-03 23:52:40 +0100

    improve error handling

--- src/main/java/fr/modcraftmc/libs/errors/ErrorsHandler.java
+++ src/main/java/fr/modcraftmc/libs/errors/ErrorsHandler.java
@@ -6,19 +6,40 @@ import javafx.scene.control.Alert;
 
 public class ErrorsHandler {
 
-    public static void handleError(Exception exception) {
+    public static void handleErrorAndCrashApplication(Exception exception) {
         ModcraftApplication.LOGGER.severe("Error thrown ! : " + exception.getMessage());
         exception.printStackTrace();
+        Alert alert = new Alert(Alert.AlertType.ERROR, exception.getMessage());
+        alert.setTitle("ModcraftMC");
+        alert.setHeaderText("Une erreur est survenue");
         Platform.runLater(() -> {
-            Alert alert = new Alert(Alert.AlertType.ERROR, exception.getMessage());
-            alert.setTitle("ModcraftMC");
-            alert.setHeaderText("Une erreur est survenue");
             alert.showAndWait();
             Platform.exit();
         });
     }
 
+    public static void handleErrorWithCustomHeaderAndCrashApplication(String header, Exception exception) {
+        ModcraftApplication.LOGGER.severe("Error thrown ! : " + exception.getMessage());
+        exception.printStackTrace();
+        Alert alert = new Alert(Alert.AlertType.ERROR, exception.getMessage());
+        alert.setTitle("ModcraftMC");
+        alert.setHeaderText(header);
+        Platform.runLater(() -> {
+            alert.showAndWait();
+            Platform.exit();
+        });
+    }
+
+    public static void handleError(Exception exception) {
+        ModcraftApplication.LOGGER.severe("Error thrown ! : " + exception.getMessage());
+        Alert alert = new Alert(Alert.AlertType.ERROR, exception.getMessage());
+        alert.setTitle("ModcraftMC");
+        alert.setHeaderText("Une erreur est survenue");
+        Platform.runLater(alert::show);
+    }
+
     public static void handleErrorWithCustomHeader(String header, Exception exception) {
+        ModcraftApplication.LOGGER.severe("Error thrown ! : " + exception.getMessage());
         Alert alert = new Alert(Alert.AlertType.ERROR, exception.getMessage());
         alert.setTitle("ModcraftMC");
         alert.setHeaderText(header);
