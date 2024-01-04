0b64430d6c71a4cb76840335bf406255cffb0d41
manugame <manugameur.pro@gmail.com>
2024-01-05 00:49:08 +0100

    use dev.modcraftmc.fr

--- src/main/java/fr/modcraftmc/launcher/SelfUpdater.java
+++ src/main/java/fr/modcraftmc/launcher/SelfUpdater.java
@@ -7,9 +7,7 @@ public class SelfUpdater {
     public record SelfUpdateResult(boolean hasUpdate, String currentVersion, String version, String changelogUrl) {};
     public static CompletableFuture<SelfUpdateResult> checkUpdate() {
         return CompletableFuture.supplyAsync(() -> {
-
-
-
+            Utils.selfCatchSleep(1500);
             return new SelfUpdateResult(false, "1.0.0", "1.0.1", "https://www.youtube.com/watch?v=xvFZjo5PgG0");
         });
     }
