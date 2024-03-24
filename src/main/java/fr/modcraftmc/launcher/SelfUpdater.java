59efd5d4de1a4d3f029bb47eef4f92b96e45258a
manugame <manugameur.pro@gmail.com>
2024-03-24 21:50:01 +0100

    disable update check on startup

--- src/main/java/fr/modcraftmc/launcher/SelfUpdater.java
+++ src/main/java/fr/modcraftmc/launcher/SelfUpdater.java
@@ -4,11 +4,19 @@ import java.util.concurrent.CompletableFuture;
 
 public class SelfUpdater {
 
-    public record SelfUpdateResult(boolean hasUpdate, String currentVersion, String version, String changelogUrl) {};
+    private static String bootstrapPath = System.getProperty("bootstrapPath");
+
+    public record SelfUpdateResult(boolean hasUpdate, String currentVersion, String version, String changelogUrl, String bootstrapPath) {};
     public static CompletableFuture<SelfUpdateResult> checkUpdate() {
+
+        if (bootstrapPath == null)
+            ModcraftApplication.LOGGER.warning("bootstrapPath is empty!! this is fine if running in dev env.");
+        else
+            ModcraftApplication.LOGGER.info("current bootstrapPath : " + bootstrapPath);
+
         return CompletableFuture.supplyAsync(() -> {
             Utils.selfCatchSleep(1500);
-            return new SelfUpdateResult(false, "1.0.0", "1.0.1", "https://www.youtube.com/watch?v=xvFZjo5PgG0");
+            return new SelfUpdateResult(false, "1.0.0", "1.0.1", "https://www.youtube.com/watch?v=xvFZjo5PgG0", bootstrapPath);
         });
     }
 }
