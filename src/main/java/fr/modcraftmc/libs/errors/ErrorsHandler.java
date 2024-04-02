8751f62a74a4b315a7c30eeaa654e517d430819e
goldor <dimgoldoritri@gmail.com>
2024-04-02 23:43:05 +0200

    add stacktrace to logs

--- src/main/java/fr/modcraftmc/libs/errors/ErrorsHandler.java
+++ src/main/java/fr/modcraftmc/libs/errors/ErrorsHandler.java
@@ -4,11 +4,13 @@ import fr.modcraftmc.launcher.ModcraftApplication;
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
 
+import java.io.StringWriter;
+
 public class ErrorsHandler {
 
     //TODO: use popup builder
     public static void handleErrorAndCrashApplication(Exception exception) {
-        ModcraftApplication.LOGGER.severe("Error thrown ! : " + exception.getMessage());
+        logException(exception);
         Platform.runLater(() -> {
             Alert alert = new Alert(Alert.AlertType.ERROR, exception.getMessage());
             alert.setTitle("ModcraftMC");
@@ -19,8 +21,7 @@ public class ErrorsHandler {
     }
 
     public static void handleErrorWithCustomHeaderAndCrashApplication(String header, Exception exception) {
-        ModcraftApplication.LOGGER.severe("Error thrown ! : " + exception.getMessage());
-        exception.printStackTrace();
+        logException(exception);
         Platform.runLater(() -> {
             Alert alert = new Alert(Alert.AlertType.ERROR, exception.getMessage());
             alert.setTitle("ModcraftMC");
@@ -31,8 +32,7 @@ public class ErrorsHandler {
     }
 
     public static void handleError(Exception exception) {
-        ModcraftApplication.LOGGER.severe("Error thrown ! : " + exception.getMessage());
-        exception.printStackTrace();
+        logException(exception);
         Platform.runLater(() -> {
             Alert alert = new Alert(Alert.AlertType.ERROR, exception.getMessage());
             alert.setTitle("ModcraftMC");
@@ -42,13 +42,18 @@ public class ErrorsHandler {
     }
 
     public static void handleErrorWithCustomHeader(String header, Exception exception) {
-        ModcraftApplication.LOGGER.severe("Error thrown ! : " + exception.getMessage());
-        exception.printStackTrace();
+        logException(exception);
         Platform.runLater(() -> {
             Alert alert = new Alert(Alert.AlertType.ERROR, exception.getMessage());
             alert.setTitle("ModcraftMC");
             alert.setHeaderText(header);
             alert.show();
         });
+    }
+
+    public static void logException(Exception exception) {
+        StringWriter sw = new StringWriter();
+        exception.printStackTrace(new java.io.PrintWriter(sw));
+        ModcraftApplication.LOGGER.severe(sw.toString());
     }
 }
