063a4785e39fc6c96d152c796e41d258f4b49a94
manugame <manugameur.pro@gmail.com>
2024-06-23 19:27:53 +0200

    Debug: add log button

--- src/main/java/fr/modcraftmc/launcher/controllers/settings/DebugController.java
+++ src/main/java/fr/modcraftmc/launcher/controllers/settings/DebugController.java
@@ -4,7 +4,12 @@ import fr.modcraftmc.launcher.ModcraftApplication;
 import fr.modcraftmc.launcher.controllers.BaseController;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class DebugController extends BaseController {
 
@@ -12,10 +17,28 @@ public class DebugController extends BaseController {
     private Label buildType;
     @FXML
     private Label version;
+    @FXML
+    public Button logBtn;
 
     @Override
     public void initialize(FXMLLoader loader) {
         buildType.setText("Build Type: " + ModcraftApplication.ENVIRONMENT.name());
         version.setText("Build Time: " + ModcraftApplication.BUILD_TIME);
+
+        logBtn.setOnMouseClicked((event) -> {
+            File instance = ModcraftApplication.filesManager.getInstancesPath();
+            if (ModcraftApplication.launcherConfig.getInstanceProperty().customInstance())
+                instance = new File(ModcraftApplication.launcherConfig.getInstanceProperty().customInstancePath());
+
+            File logdirectory = new File(instance, "logs");
+            if (!logdirectory.exists())
+                logdirectory.mkdirs();
+
+            try {
+                Desktop.getDesktop().open(logdirectory);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
