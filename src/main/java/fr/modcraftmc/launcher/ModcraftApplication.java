c6e813f6edb2c2b463eac16621545527ec0518ab
manugame <braibant.manu@gmail.com>
2025-05-21 09:54:12 +0200

    chore(GameLauncher): bump neoforge version to 21.1.172

--- src/main/java/fr/modcraftmc/launcher/ModcraftApplication.java
+++ src/main/java/fr/modcraftmc/launcher/ModcraftApplication.java
@@ -42,7 +42,7 @@ public class ModcraftApplication extends Application {
 
     //Constants
     public static String BUILD_TIME = "DEV";
-    public static String NEOFORGE_VERSION = "21.1.169";
+    public static String NEOFORGE_VERSION = "21.1.172";
     public static String MC_VERSION = "1.21.1";
     public static ModcraftApplication app;
     private static Stage window;
